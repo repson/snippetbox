edc7c0905952fb2266ec1170fd1b0905febc6bc5
Isaac Peña <isaac@repson.org>
2023-06-11 16:28:18 +0200

    mux instantiation added

--- main.go
+++ main.go
@@ -23,11 +23,12 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/snippet/view", snippetView)
-	http.HandleFunc("/snippet/create", snippetCreate)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on localhost:4000")
-	err := http.ListenAndServe("localhost:4000", nil)
+	err := http.ListenAndServe("localhost:4000", mux)
 	log.Fatal(err)
 }
