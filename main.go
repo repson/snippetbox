71beff7c1fe69a993ee0b71b8bcb5ae8144eb518
Isaac Peña <isaac@repson.org>
2023-06-11 16:58:08 +0200

    Using constants from the net/http package for HTTP methods and status codes

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", 405)
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
