b971c8fc92f25da8ade4d88842b0b436fafd2b8f
Isaac Peña <isaac@repson.org>
2023-06-26 22:06:01 +0200

    bug fixed

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -74,10 +74,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := &templateData{
-		Snippet: snippet
+		Snippet: snippet,
 	}
 
-	err = ts.ExecuteTemplate(w, "base", snippet)
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
