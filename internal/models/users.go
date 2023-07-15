f798313fadcb373f2bbd96efb813de923d216a62
Isaac Peña <info@ipenat.com>
2023-07-15 19:21:53 +0200

    insert function in users.go implemented

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -18,7 +23,24 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
-	return nil
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return errors.New("duplicate email")
+			}
+		}
+	}
+
+	return err
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
