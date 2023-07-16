3ecd3d3fa2decbced3ed42ea49e9bd6e4aba647b
Isaac Peña <info@ipenat.com>
2023-07-16 19:40:55 +0200

    newTemplateData functionand nav.tmpl templated have been updated

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -33,8 +33,9 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		isAuthenticated: app.isAuthenticated(r),
 	}
 }
 
