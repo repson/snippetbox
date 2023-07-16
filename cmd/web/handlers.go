8e965d086b029d8c72cbe29078e4d8393813a579
Isaac Peña <info@ipenat.com>
2023-07-16 19:33:25 +0200

    userLogoutPost function implemented

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -213,5 +213,15 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.Destroy(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
