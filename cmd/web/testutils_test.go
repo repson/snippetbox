cdcc90e8cea7fe20a7d69f60d680d85d0034e94f
Isaac Peña <info@ipenat.com>
2023-07-22 19:17:24 +0200

    bug fixed

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -38,5 +38,5 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	}
 	bytes.TrimSpace(body)
 
-	return rs.StatusCode, rs.Header, body
+	return rs.StatusCode, rs.Header, string(body)
 }
